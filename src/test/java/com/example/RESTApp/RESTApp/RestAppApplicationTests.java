d8ec62b0c6a7818bc0fbc8a52c9ccd7e8d87db91
unknown <abhinandan8296@gmail.com>
2023-04-17 00:53:04 +0530

    Spring boot starter

---
+++ src/test/java/com/example/RESTApp/RESTApp/RestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RESTApp.RESTApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
